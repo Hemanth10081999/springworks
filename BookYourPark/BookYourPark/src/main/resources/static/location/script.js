e5da88231123c1989d16e16d5a75f16831ad6384
teamdriptech <vhemantharajan@gmail.com>
2019-12-24 22:49:35 +0530

    adding gps based location search

--- BookYourPark/BookYourPark/src/main/resources/static/location/script.js
+++ BookYourPark/BookYourPark/src/main/resources/static/location/script.js
@@ -8,7 +8,21 @@ function logout() {
     window.location = "../home/index.html";
 }
 
+
+
+
+
 function loadlocation() {
+
+    //get user location for map data
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition); //position.coords.latitude, position.coords.longitude  variables for getting cooordinates
+
+    } else {
+        alert("geo location is not suported by your browser");
+    }
+
+
 
 
     document.cookie = "location=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
@@ -26,10 +40,8 @@ function loadlocation() {
                 document.getElementById('posts').innerHTML += `
 
                 <tr onclick="locClick(${p.locId})">
-                <td>${x++}</td>
                 <td>${p.locName}</td>
                 <td>${p.locAddress}</td>
-                <td>${p.totalSlots}</td>
                 <td>${p.availableSlots}</td>
                 </tr>
                 
@@ -44,22 +56,43 @@ function loadlocation() {
 }
 
 
+function showPosition(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+    initMaps(position.coords.latitude, position.coords.longitude);
+
+}
+
+
+
+function initMaps(latitude, longitude) {
+
+    var newlatitude = latitude.toFixed(6);
+    var newlongitude = longitude.toFixed(6);
+
+
+    var mapProp = {
+        center: new google.maps.LatLng(parseFloat(newlatitude), parseFloat(newlongitude)),
+        zoom: 17,
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var marker = new google.maps.Marker({ position: new google.maps.LatLng(parseFloat(newlatitude), parseFloat(newlongitude)), map: map });
+
+
+}
+
+
 function sorttable() {
 
     var sort = document.getElementById('myList');
     var strSel = sort.options[sort.selectedIndex].value;
-    console.log('entered into click');
 
     if (strSel != "") {
-
-
 
         let element = document.getElementById("posts");
         while (element.firstChild) {
             element.removeChild(element.firstChild);
         }
-
-
         const Data = {
             url: 'http://localhost:8080/api/locations/sort',
             data: {
@@ -67,25 +100,17 @@ function sorttable() {
             }
         };
         postData(Data.url, Data.data)
-
-        .then(posts => {
-
-                var x = 1;
+            .then(posts => {
 
                 posts.map(p => {
-
-
+                    console.log("entered");
                     document.getElementById('posts').innerHTML += `
             
             <tr onclick="locClick(${p.locId})">
-            <td>${x++}</td>
             <td>${p.locName}</td>
             <td>${p.locAddress}</td>
-            <td>${p.totalSlots}</td>
             <td>${p.availableSlots}</td>
             </tr>
-            
-            
             `;
                     console.log(p.title)
                 });
@@ -94,8 +119,42 @@ function sorttable() {
                 console.log(err);
             });
     } else {
-        document.getElementById('posts').innerHTML = ``;
-        loadlocation();
+
+
+        let element = document.getElementById("posts");
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+
+
+        document.cookie = "location=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+        fetch('http://localhost:8080/api/locations')
+            .then((res) => res.json())
+            .then(posts => {
+
+                let output = '';
+                var x = 1;
+
+                posts.map(p => {
+
+
+                    document.getElementById('posts').innerHTML += `
+    
+                    <tr onclick="locClick(${p.locId})">
+                    <td>${p.locName}</td>
+                    <td>${p.locAddress}</td>
+                    <td>${p.availableSlots}</td>
+                    </tr>
+                    
+                    
+                    `;
+                    console.log(p.title)
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 }
 
