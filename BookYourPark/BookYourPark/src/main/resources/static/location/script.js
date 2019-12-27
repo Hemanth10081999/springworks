03ffb9cf9a2811398a5055edc1735eb2c3811e95
teamdriptech <vhemantharajan@gmail.com>
2019-12-27 12:16:35 +0530

    adding markers for locations

--- BookYourPark/BookYourPark/src/main/resources/static/location/script.js
+++ BookYourPark/BookYourPark/src/main/resources/static/location/script.js
@@ -9,21 +9,10 @@ function logout() {
 }
 
 
-
-
+var locations = [];
+var markers = [];
 
 function loadlocation() {
-
-    //get user location for map data
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition); //position.coords.latitude, position.coords.longitude  variables for getting cooordinates
-
-    } else {
-        alert("geo location is not suported by your browser");
-    }
-
-
-
 
     document.cookie = "location=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
@@ -32,7 +21,7 @@ function loadlocation() {
         .then(posts => {
 
             let output = '';
-            var x = 1;
+            var x = 0;
 
             posts.map(p => {
 
@@ -44,42 +33,67 @@ function loadlocation() {
                 <td>${p.locAddress}</td>
                 <td>${p.availableSlots}</td>
                 </tr>
-                
-                
                 `;
-                console.log(p.title)
+
+                locations[x] = new google.maps.LatLng({ lat: parseFloat(p.latitude), lng: parseFloat(p.longitude) });
+                x = x + 1;
+
             });
         })
         .catch((err) => {
             console.log(err);
         });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition); //position.coords.latitude, position.coords.longitude  variables for getting cooordinates
+
+    } else {
+        alert("geo location is not suported by your browser");
+    }
 }
+
+
 
 
 function showPosition(position) {
     console.log(position.coords.latitude);
     console.log(position.coords.longitude);
-    initMaps(position.coords.latitude, position.coords.longitude);
-
+    setTimeout(function() {
+        initMaps(position.coords.latitude, position.coords.longitude);
+    }, 3000);
 }
 
 
 
 function initMaps(latitude, longitude) {
-
     var newlatitude = latitude.toFixed(6);
     var newlongitude = longitude.toFixed(6);
-
-
     var mapProp = {
-        center: new google.maps.LatLng(parseFloat(newlatitude), parseFloat(newlongitude)),
-        zoom: 17,
+        center: new google.maps.LatLng(parseFloat(13.068509), parseFloat(80.220951)),
+        zoom: 16,
     };
     var map = new google.maps.Map(document.getElementById("map"), mapProp);
     var marker = new google.maps.Marker({ position: new google.maps.LatLng(parseFloat(newlatitude), parseFloat(newlongitude)), map: map });
+    console.log(locations.length);
+    for (var i = 0; i < locations.length; i++) {
+        console.log(locations[i]);
+        markers.push(new google.maps.Marker({
+            position: locations[i],
+            map: map,
+            animation: google.maps.Animation.DROP
+        }));
 
-
+    }
 }
+
+
+function clearMarkers() {
+    for (var i = 0; i < locations.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+}
+
 
 
 function sorttable() {
@@ -88,6 +102,12 @@ function sorttable() {
     var strSel = sort.options[sort.selectedIndex].value;
 
     if (strSel != "") {
+
+        clearMarkers();
+        for (var i = 0; i < locations.length; i++) {
+            locations.pop();
+        }
+
 
         let element = document.getElementById("posts");
         while (element.firstChild) {
@@ -101,60 +121,39 @@ function sorttable() {
         };
         postData(Data.url, Data.data)
             .then(posts => {
-
+                var x = 0;
                 posts.map(p => {
                     console.log("entered");
                     document.getElementById('posts').innerHTML += `
-            
+                    
             <tr onclick="locClick(${p.locId})">
             <td>${p.locName}</td>
             <td>${p.locAddress}</td>
             <td>${p.availableSlots}</td>
             </tr>
             `;
-                    console.log(p.title)
+                    locations[x] = new google.maps.LatLng({ lat: parseFloat(p.latitude), lng: parseFloat(p.longitude) });
+                    x = x + 1;
                 });
             })
             .catch((err) => {
                 console.log(err);
             });
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition); //position.coords.latitude, position.coords.longitude  variables for getting cooordinates
+
+        } else {
+            alert("geo location is not suported by your browser");
+        }
+
+
     } else {
-
-
+        clearMarkers();
         let element = document.getElementById("posts");
         while (element.firstChild) {
             element.removeChild(element.firstChild);
         }
-
-
-        document.cookie = "location=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-
-        fetch('http://localhost:8080/api/locations')
-            .then((res) => res.json())
-            .then(posts => {
-
-                let output = '';
-                var x = 1;
-
-                posts.map(p => {
-
-
-                    document.getElementById('posts').innerHTML += `
-    
-                    <tr onclick="locClick(${p.locId})">
-                    <td>${p.locName}</td>
-                    <td>${p.locAddress}</td>
-                    <td>${p.availableSlots}</td>
-                    </tr>
-                    
-                    
-                    `;
-                    console.log(p.title)
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        loadlocation();
     }
 }
 
